22493014e5f3c85e5c03fc24cc1bc2200ac12205
venkata16sidhartha <46317898+venkata16sidhartha@users.noreply.github.com>
2021-10-27 14:50:30 +0530

    Create client.sh

---
+++ client.sh
@@ -0,0 +1,28 @@
+client="$1"
+
+new_client () {
+	# Generates the custom client.ovpn
+	{
+	cat /etc/openvpn/server/client-common.txt
+	echo "dhcp-option DNS 10.1.1.5"
+	echo "dhcp-option DNS 8.8.8.8"
+	echo "<ca>"
+	cat /etc/openvpn/server/easy-rsa/pki/ca.crt
+	echo "</ca>"
+	echo "<cert>"
+	sed -ne '/BEGIN CERTIFICATE/,$ p' /etc/openvpn/server/easy-rsa/pki/issued/"$client".crt
+	echo "</cert>"
+	echo "<key>"
+	cat /etc/openvpn/server/easy-rsa/pki/private/"$client".key
+	echo "</key>"
+	echo "<tls-crypt>"
+	sed -ne '/BEGIN OpenVPN Static key/,$ p' /etc/openvpn/server/tc.key
+	echo "</tls-crypt>"
+	} > ~/openvpn/"$client".ovpn
+}
+
+
+cd /etc/openvpn/server/easy-rsa/
+EASYRSA_CERT_EXPIRE=3650 ./easyrsa build-client-full "$client" nopass
+# Generates the custom client.ovpn
+new_client
